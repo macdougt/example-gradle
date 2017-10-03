c099e3ed45d2ad905515464589a5438a48b4a700
Tom MacDougall <macdougt71@gmail.com>
2017-10-03 09:28:49 -0400

    test

--- src/main/java/org/jacoco/examples/java/gradle/HelloWorld.java
+++ src/main/java/org/jacoco/examples/java/gradle/HelloWorld.java
@@ -3,6 +3,7 @@ package org.jacoco.examples.java.gradle;
 public class HelloWorld {
 	
 	public String getMessage(boolean bigger) {
+                System.out.println("Here I am");
 		if (bigger) {
 			return "Hello Universe!";
 		} else {
